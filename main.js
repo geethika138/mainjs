a1722c030b94b0b62dea2a4a50d563b0e74a8800
geethika138 <147244214+geethika138@users.noreply.github.com>
2024-08-28 16:24:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import chalk from 'chalk'
+import ConnectDB from './config/db.js'
+
+dotenv.config() 
+
+ConnectDB()
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello World</h1>')
+})
+
+
+app.listen(5050,(req,res)=>{
+    console.log(chalk.bgRed('Server Running'))
+})
